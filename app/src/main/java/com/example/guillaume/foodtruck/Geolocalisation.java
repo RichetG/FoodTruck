256809f9ffb911daef8a5e8915f1172a66b0bb47
guillaume <richet.guillaume@gmail.com>
2015-03-07 16:57:59 +0100

    enfin la geolocalisation fonctionne

--- app/src/main/java/com/example/guillaume/foodtruck/Geolocalisation.java
+++ app/src/main/java/com/example/guillaume/foodtruck/Geolocalisation.java
@@ -1,43 +1,78 @@
 package com.example.guillaume.foodtruck;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.IntentSender;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-import com.google.android.gms.maps.*;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/*public class Geolocalisation extends FragmentActivity {
+public class Geolocalisation extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
-    private GoogleMap map;
-    private Marker marker;
+    private GoogleApiClient mGoogleApiClient;
+    public static final String TAG=Geolocalisation.class.getSimpleName();
+    private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST=9000;
+    private LocationRequest mLocationRequest;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.geolocalisation);
         setUpMapIfNeeded();
+        mGoogleApiClient=new GoogleApiClient.Builder(this).addConnectionCallbacks(this).addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        mLocationRequest=LocationRequest.create().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY).setInterval(10*1000).setFastestInterval(1000);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+        mGoogleApiClient.connect();
     }
 
+    @Override
+    protected  void onPause() {
+        super.onPause();
+        if(mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
-        if (map == null) {
+        if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
             // Check if we were successful in obtaining the map.
-            if (map != null) {
+            if (mMap != null) {
                 setUpMap();
             }
         }
@@ -47,98 +82,54 @@ import com.google.android.gms.maps.model.MarkerOptions;
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
      * <p/>
-     * This should only be called once and when we are sure that {@link #map} is not null.
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
-  /*  private void setUpMap() {
-        marker=map.addMarker(new MarkerOptions().title("Vous êtes ici").position(new LatLng(48.856614, 2.3522219000000177)));
+    private void setUpMap() {
 
-
-    }
-}*/
-
-
-/**
- * Created by guillaume on 07/02/15.
- */
-public class Geolocalisation extends Activity implements LocationListener {
-
-    private GoogleMap map;
-    private LocationManager locationManager;
-    private Marker marker;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.geolocalisation);
-
-        map= ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
-        marker = map.addMarker(new MarkerOptions().title("Localisation par défaut").position(new LatLng(48.856614, 2.3522219000000177)));
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Services de localisation connectés");
+        Location location=LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(location==null){
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }else{
+            handleNewLocation(location);
+        }
+    }
 
-        //Obtention de la référence du service
-       locationManager= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    private void handleNewLocation(Location location){
+        Log.d(TAG, location.toString());
+        double currentLatitude=location.getLatitude();
+        double currentLongitude=location.getLongitude();
+        LatLng latLng=new LatLng(currentLatitude, currentLongitude);
+        MarkerOptions options=new MarkerOptions().position(latLng).title("Vous êtes ici");
+        mMap.addMarker(options);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
 
-        //Si le GPS est disponible, on s'y abonne
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            abonnementGPS();
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Services de localisation suspendus. S'il vous plaît veuillez vous reconnecté.");
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(connectionResult.hasResolution()){
+            try{
+                //demarrage de l'activité si essaye de resoudre une erreur
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            }catch(IntentSender.SendIntentException e){
+                e.printStackTrace();
+            }
+        }else{
+            Log.i(TAG, "Connexion des services de localisation échoué avec le code "+connectionResult.getErrorCode());
         }
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-
-        //On appelle la méthode pour se désabonner
-        desabonnementGPS();
-    }
-
-    /**
-     * Méthode permettant de s'abonner à la localisation par GPS.
-     */
-    public void abonnementGPS() {
-        //On s'abonne
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
-    }
-
-    /**
-     * Méthode permettant de se désabonner de la localisation par GPS.
-     */
-    public void desabonnementGPS() {
-        //Si le GPS est disponible, on s'y abonne
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onLocationChanged(final Location location) {
-     //Mise à jour des coordonnées
-     /*   final LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-        MarkerOptions option=new MarkerOptions().position(latLng).title("Vous êtes ici");
-        map.addMarker(option);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));*/
-    }
-
-    @Override
-    public void onProviderDisabled(final String provider) {
-        //Si le GPS est désactivé on se désabonne
-     /*   if("gps".equals(provider)) {
-            desabonnementGPS();
-        }*/
-    }
-
-    @Override
-    public void onProviderEnabled(final String provider) {
-        //Si le GPS est activé on s'abonne
-       /* if("gps".equals(provider)) {
-            abonnementGPS();
-        }*/
-    }
-
-    @Override
-    public void onStatusChanged(final String provider, final int status, final Bundle extras) {
-
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
     }
 }
